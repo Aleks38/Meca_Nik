06c5c4af0a71739605ded0936b9be0b86a95732e
Aleks38 <alexy.dasilva@icloud.com>
2022-10-04 11:41:19 +0200

    Page ajout d'utilisateur

---
+++ createUser.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Meac-Nik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+        
+
+
+                <div class="container-fluid col-5">
+                    <form>
+                        <div class="row">
+                            <!-- Name input -->
+                            <div class="col-sm-6">
+                                <input type="text" id="form3Example3" class="form-control" placeholder="Nom" />
+                            </div>
+
+                            <!-- Firstname input -->
+                            <div class="col-sm-6">
+                                <input type="text" id="form3Example4" class="form-control" placeholder="Prénom" />
+                                
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!-- Username input -->
+                            <div class="col-sm-6">
+                                <input type="text" id="form3UserName" class="form-control" placeholder="Nom d'utilisateur"/>
+                            </div>
+
+                            <!-- Role input -->
+                            <div class="col-sm-6">
+                                <select class="form-select">
+                                    <option selected>Rôle...</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+       
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
